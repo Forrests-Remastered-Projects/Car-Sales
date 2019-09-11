8ca3192b8ab9a6ae7bf8e824f1a859f080c3b014
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-09-11 14:41:04 -0700

    App is done, finishing reducer

--- src/App.js
+++ src/App.js
@@ -30,28 +30,7 @@ const mapStateToProps = state => {
   };
 };
 
-const initialState = {
-  additionalPrice: 0,
-  car: {
-    price: 26395,
-    name: "2019 Ford Mustang",
-    image:
-      "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
-    features: []
-  },
-  store: [
-    { id: 1, name: "V-6 engine", price: 1500 },
-    { id: 2, name: "Racing detail package", price: 1500 },
-    { id: 3, name: "Premium sound system", price: 500 },
-    { id: 4, name: "Rear spoiler", price: 250 }
-  ]
-};
-const removeFeature = item => {
-  // dispatch an action here to remove an item
-};
-
-const buyItem = item => {
-  // dipsatch an action here to add an item
-};
-
-export default App;
+export default connect(
+  mapStateToProps,
+  { buyFeature, removeFeature }
+)(App);
